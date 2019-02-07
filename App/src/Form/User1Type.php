3cb6106ce6fa0759b7ff3c51c21ace6618c709a0
walidmh <walid_mohsin10@hotmail.com>
2019-02-07 20:12:07 +0100

    test
    Signed-off-by: walidmh <walid_mohsin10@hotmail.com>

---
+++ App/src/Form/User1Type.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class User1Type extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('roles')
+            ->add('password')
+            ->add('nomUser')
+            ->add('prenomUser')
+            ->add('telephoneUser')
+            ->add('adresseUser')
+            ->add('codePostalUser')
+            ->add('ville')
+            ->add('pays')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
